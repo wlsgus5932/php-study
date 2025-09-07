b2b26976f616ebe39665b59fcdc02ba66bb52f27
jinhyun <wlsgus1149@naver.com>
2025-09-07 20:22:54 +0900

    반복문

---
+++ 8.php
@@ -0,0 +1,13 @@
+<?php
+/* 반복문 */
+
+// while(조건문) {실행문;}
+// for(초기값; 조건문; 증감식) {실행문;}
+// do {실행문;} while(조건문); //최소 1회 실행
+// foreach(배열 as $value) {실행문;}
+// foreach(배열 as $key => $value) {실행문;}
+// key value 선언법
+// $arr = array("key1" => "value1", "key2" => "value2");
+// break;, continue; 개념 똑같음
+
+?>
